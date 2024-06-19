1bbe839cab5fabffb35fc3548d1057ca5feb5622
Darppan <darppan524@gmail.com>
2024-06-19 18:04:29 +0530

    commit2

---
+++ gausssiedel.f90
@@ -0,0 +1,22 @@
+program euler
+    implicit none
+    real :: xi,yi,zi,xi1,yi1,zi1,epsilon
+    integer :: i
+    xi=0.0
+    yi=0.0
+    zi=0.0
+    epsilon=1
+    i=0
+    do while(epsilon>0.00001)
+        xi1=(3+yi+zi)/4
+        yi1=(9+2*xi1-zi)/6
+        zi1=(xi1-yi1-6)/7
+        epsilon=((xi1-xi)**2+(yi1-yi)**2+(zi1-zi)**2)**0.5
+        xi=xi1
+        yi=yi1
+        zi=zi1
+        i=i+1
+    end do
+    print*, "iterations:",i
+    print*, xi," ", yi," ", zi
+end program euler
